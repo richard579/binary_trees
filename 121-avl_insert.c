b498d5bdbf97ddb5ee8663c11490ecc170ea38e7
richard579 <richardbeng358@outlook.com>
2023-08-03 08:26:07 -0700

    121-avl_insert.c

--- 121-avl_insert.c
+++ 121-avl_insert.c
@@ -1,7 +1,7 @@
 #include "binary_trees.h"
 
 size_t height(const binary_tree_t *tree);
-int balance(const binary_tree_t *tree;
+int balance(const binary_tree_t *tree);
 avl_t *avl_insert_recursive(avl_t **tree, avl_t *parent,
 		avl_t **new, int value);
 avl_t *avl_insert(avl_t **tree, int value);
